24f247cd22a56c6a47cb031beaebdf9b9c8eba32
Gaurav <gauravsaxena5000@gmail.com>
2025-03-27 18:31:13 +0530

    Initial files

---
+++ warrior/src/Components/welcome text/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+        <h1>WELCOME TO RCSAS COMPUTER EDUCATION FRANCHISE</h1>
+        <p>
+        RCSAS Education provides Best Computer Education Franchise Business opportunity in India to help people, who interested to start your own business and take a Low Cost Franchise. It is one of the Top Computer Education Franchise Business in India.
+We offers more advanced IT education courses that are synchronized with advanced technological revolution at global level.
+We offer various courses including Computer Courses, Optical Courses, Hardware Courses, Associate Courses, Tailoring Courses, Nursery Courses, Technical Courses, Fire and Safety Courses, Other Courses, Cinematic Courses, Food Courses, Beauty and Hair Courses, Solar Courses, Medical Courses, Agricultural Courses, Insurance Courses, Diamond Courses, Fashion Designing, Mobile Repairing Courses, Banking Courses, Special Courses too in the field Information Technology.
+RCSAS Education facilitate the creation of a network of Computer Course Franchise Business worldwide to empower the workforce with job oriented skills. We provide Best Low Cost Franchises Business in the market. Our franchise programs are customized keeping market trend in mind, thereby we are developing talent in the student.
+RCSAS Education provides the opportunity to start your own business in computer education, and offer career-training courses in the above listed industries. Selected business franchise partner that shares RCSAS passion and vision to spared in remote area. We are a market-focused, organization that develops and delivers solutions to our Franchise partner.
+        </p>
+    </div>
+  )
+}
+
+export default Welcome
