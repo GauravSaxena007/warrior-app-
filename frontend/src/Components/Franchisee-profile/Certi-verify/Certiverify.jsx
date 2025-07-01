d081d0501a53c19612b07f1004c5c4558c1807d4
Gaurav <gauravsaxena5000@gmail.com>
2025-07-01 22:01:37 +0530

    🔥 Rebuilt frontend with latest Certiverify updates

--- frontend/src/Components/Franchisee-profile/Certi-verify/Certiverify.jsx
+++ frontend/src/Components/Franchisee-profile/Certi-verify/Certiverify.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Certiverify.css';
 
+
 const Certiverify = () => {
   const [certificateNumber, setCertificateNumber] = useState('');
   const [matchedCertificate, setMatchedCertificate] = useState(null);
